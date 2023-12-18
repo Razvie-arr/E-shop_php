bd06b7cc802a07ae642af8a3a30a95a32329fb33
Razvie-arr <vladikbyastinov@gmail.com>
2023-12-19 00:09:15 +0100

    delete dashboard from admin module

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -7,25 +7,25 @@ namespace App\Router;
 use Nette;
 use Nette\Application\Routers\RouteList;
 
-final class RouterFactory{
-	use Nette\StaticClass;
+final class RouterFactory {
+    use Nette\StaticClass;
 
-	public static function createRouter():RouteList {
+    public static function createRouter(): RouteList {
 
-    $adminRouter = new RouteList('Admin');
-    $adminRouter->addRoute('admin/<presenter=Dashboard>/<action=default>[/<id>]');
+        $adminRouter = new RouteList('Admin');
+        $adminRouter->addRoute('admin/<presenter=Product>/<action=default>[/<id>]');
 
-	  $frontRouter = new RouteList('Front');
-    $frontRouter->addRoute('produkty', 'Product:list');  //pokud je do adresy zakomponována také proměnná category, je doplněna do adresy
-    $frontRouter->addRoute('kontakty','Contact:default');
-    $frontRouter->addRoute('onas','About:default');
-    $frontRouter->addRoute('sitemap.xml', 'Homepage:sitemap');
-    $frontRouter->addRoute('produkty/<url>', 'Product:show');  //pokud je do adresy zakomponována také proměnná category, je doplněna prostřední část adresy
-		$frontRouter->addRoute('<presenter=Homepage>/<action=default>[/<id>]');
+        $frontRouter = new RouteList('Front');
+        $frontRouter->addRoute('produkty', 'Product:list');  //pokud je do adresy zakomponována také proměnná category, je doplněna do adresy
+        $frontRouter->addRoute('kontakty', 'Contact:default');
+        $frontRouter->addRoute('onas', 'About:default');
+        $frontRouter->addRoute('sitemap.xml', 'Homepage:sitemap');
+        $frontRouter->addRoute('produkty/<url>', 'Product:show');  //pokud je do adresy zakomponována také proměnná category, je doplněna prostřední část adresy
+        $frontRouter->addRoute('<presenter=Homepage>/<action=default>[/<id>]');
 
-	  $router = new RouteList();
-		$router->add($adminRouter);
-		$router->add($frontRouter);
-		return $router;
-	}
+        $router = new RouteList();
+        $router->add($adminRouter);
+        $router->add($frontRouter);
+        return $router;
+    }
 }
