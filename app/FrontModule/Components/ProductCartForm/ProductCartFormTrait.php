44361b7ca119c08475709af08b733f028c685a91
Razvie-arr <vladikbyastinov@gmail.com>
2023-12-31 00:48:55 +0100

    add exception if user selects more product than in stock

--- app/FrontModule/Components/ProductCartForm/ProductCartFormTrait.php
+++ app/FrontModule/Components/ProductCartForm/ProductCartFormTrait.php
@@ -10,17 +10,26 @@ trait ProductCartFormTrait {
             $form = $this->productCartFormFactory->create();
             $form->setDefaults(['productId' => $productId]);
             $form->onSubmit[] = function (ProductCartForm $form) {
+                $selectedProductCount = (int)$form->values->count;
                 try {
                     $product = $this->productsFacade->getProduct($form->values->productId);
                     //kontrola zakoupitelnosti
+
+                    if ($selectedProductCount > $product->stock) {
+                        throw new \InvalidArgumentException();
+                    }
+                } catch (\InvalidArgumentException $e) {
+                    $this->flashMessage('Vybral jste větší množství produktů, než je skladem', 'error');
+                    $this->redirect('this');
                 } catch (\Exception $e) {
                     $this->flashMessage('Produkt nejde přidat do košíku', 'error');
                     $this->redirect('this');
                 }
+
                 //přidání do košíku
                 /** @var CartControl $cart */
                 $cart = $this->getComponent('cart');
-                $cart->addToCart($product, (int)$form->values->count);
+                $cart->addToCart($product, $selectedProductCount);
                 $this->redirect('this');
             };
 
