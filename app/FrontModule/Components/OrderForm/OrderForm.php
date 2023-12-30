38b4e728bbb1f1f9e027e0f6286d64c97893059c
Razvie-arr <vladikbyastinov@gmail.com>
2023-12-30 23:25:34 +0100

    add default user data: name and email in order form

--- app/FrontModule/Components/OrderForm/OrderForm.php
+++ app/FrontModule/Components/OrderForm/OrderForm.php
@@ -5,6 +5,7 @@ namespace App\FrontModule\Components\OrderForm;
 use Nette;
 use Nette\Application\UI\Form;
 use Nette\Forms\Controls\SubmitButton;
+use Nette\Security\User;
 use Nette\SmartObject;
 use Nextras\FormsRendering\Renderers\Bs4FormRenderer;
 use Nextras\FormsRendering\Renderers\FormLayout;
@@ -13,48 +14,53 @@ use Nextras\FormsRendering\Renderers\FormLayout;
  * Class OrderForm
  * @package App\FrontModule\Components\OrderForm
  *
- * @method onFinished($message='')
+ * @method onFinished($message = '')
  * @method onCancel()
  */
-class OrderForm extends Form{
+class OrderForm extends Form {
 
     use SmartObject;
 
+    private User $user;
     public array $onFinished = [];
     public array $onCancel = [];
 
 
     /**
      * OrderForm constructor.
+     * @param User $user
      * @param Nette\ComponentModel\IContainer|null $parent
      * @param string|null $name
      */
-    public function __construct(Nette\ComponentModel\IContainer $parent = null, string $name = null){
+    public function __construct(User $user, Nette\ComponentModel\IContainer $parent = null, string $name = null) {
         parent::__construct($parent, $name);
         $this->setRenderer(new Bs4FormRenderer(FormLayout::VERTICAL));
+        $this->user = $user;
         $this->createSubcomponents();
     }
 
 
-    private function createSubcomponents():void {
-        $this->addText('jmeno','Jméno')
-            ->setRequired('Zadejte vaše jméno');
-        $this->addText('adresa','Adresa')
+    private function createSubcomponents(): void {
+        $this->addText('jmeno', 'Jméno')
+            ->setRequired('Zadejte vaše jméno')
+            ->setDefaultValue($this->user->identity->name);
+        $this->addText('adresa', 'Adresa')
             ->setRequired('Vyplňte vaši adresu');
-        $this->addEmail('email','E-mail')
-            ->setRequired('Zadejte váš E-mail');
-        $this->addInteger('telefon','Telefonní číslo')
+        $this->addEmail('email', 'E-mail')
+            ->setRequired('Zadejte váš E-mail')
+            ->setDefaultValue($this->user->identity->email);
+        $this->addInteger('telefon', 'Telefonní číslo')
             ->setRequired('Zadejte váše číslo');
-        $this->addCheckbox('potvrzeni','Souhlasím s obchodními podmínkami a chci závazně objednat.')
+        $this->addCheckbox('potvrzeni', 'Souhlasím s obchodními podmínkami a chci závazně objednat.')
             ->setRequired('Pro pokračování musíte souhlasit');
 
-        $this->addSubmit('ok','Potvrdit objednávku')
-            ->onClick[]=function(SubmitButton $button){
+        $this->addSubmit('ok', 'Potvrdit objednávku')
+            ->onClick[] = function (SubmitButton $button) {
             $this->onFinished('Děkujeme za vaši objednávku. Vaši objednávku vyřídíme co nedjříve.');
         };
-        $this->addSubmit('storno','Zrušit')
+        $this->addSubmit('storno', 'Zrušit')
             ->setValidationScope([])
-            ->onClick[]=function(SubmitButton $button){
+            ->onClick[] = function (SubmitButton $button) {
             $this->onCancel();
         };
     }
